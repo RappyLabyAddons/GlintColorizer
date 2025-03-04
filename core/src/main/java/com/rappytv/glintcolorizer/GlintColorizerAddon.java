0d9827db5ee657a18adf6cffcfe4f836e2120fc6
RappyTV <rappytv@rappytv.com>
2025-03-04 17:46:28 +0100

    Add util functions to addon class

--- core/src/main/java/com/rappytv/glintcolorizer/GlintColorizerAddon.java
+++ core/src/main/java/com/rappytv/glintcolorizer/GlintColorizerAddon.java
@@ -2,17 +2,43 @@ package com.rappytv.glintcolorizer;
 
 import net.labymod.api.addon.LabyAddon;
 import net.labymod.api.models.addon.annotation.AddonMain;
+import net.labymod.api.util.Color;
+import org.jetbrains.annotations.Nullable;
 
 @AddonMain
 public class GlintColorizerAddon extends LabyAddon<GlintColorizerConfig> {
 
+  private static GlintColorizerAddon instance;
+
   @Override
   protected void enable() {
+    instance = this;
+
     this.registerSettingCategory();
   }
 
   @Override
   protected Class<? extends GlintColorizerConfig> configurationClass() {
     return GlintColorizerConfig.class;
+  }
+
+  public static boolean isItemGlowEnabled() {
+    return instance.configuration().enabled().get() && instance.configuration().itemGlowEffect().get();
+  }
+
+  @Nullable
+  public static Color getItemGlintColor() {
+    if(!instance.configuration().enabled().get() || !instance.configuration().itemGlintEffect().get()) {
+      return null;
+    }
+    return instance.configuration().itemGlintColor().get();
+  }
+
+  @Nullable
+  public static Color getArmorGlintColor() {
+    if(!instance.configuration().enabled().get() || !instance.configuration().armorGlintEffect().get()) {
+      return null;
+    }
+    return instance.configuration().armorGlintColor().get();
   }
 }
