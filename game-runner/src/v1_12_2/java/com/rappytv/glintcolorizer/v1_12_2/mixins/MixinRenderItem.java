e930cbb66d08537c138b719f0dac1e9b11d1f947
RappyTV <rappytv@rappytv.com>
2025-03-04 18:59:58 +0100

    Use new config options in mixin impls

--- game-runner/src/v1_12_2/java/com/rappytv/glintcolorizer/v1_12_2/mixins/MixinRenderItem.java
+++ game-runner/src/v1_12_2/java/com/rappytv/glintcolorizer/v1_12_2/mixins/MixinRenderItem.java
@@ -1,16 +1,20 @@
 package com.rappytv.glintcolorizer.v1_12_2.mixins;
 
 import com.rappytv.glintcolorizer.GlintColorizerAddon;
+import com.rappytv.glintcolorizer.GlintColorizerConfig.ItemEffect;
 import net.labymod.api.util.Color;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderItem;
+import net.minecraft.client.renderer.block.model.IBakedModel;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderItem.class)
 public class MixinRenderItem {
@@ -18,10 +22,19 @@ public class MixinRenderItem {
   @Unique
   private boolean sandbox$firstDepthCall = true;
 
+  @Inject(method = "renderEffect", at = @At("HEAD"), cancellable = true)
+  private void renderEffect(IBakedModel model, CallbackInfo ci) {
+    if(GlintColorizerAddon.getItemEffect() == ItemEffect.NONE) {
+      ci.cancel();
+    }
+  }
+
   @Redirect(method = "renderEffect", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;depthFunc(I)V"))
   private void modifyDepthFunc(int i) {
-    if(this.sandbox$firstDepthCall && GlintColorizerAddon.isItemGlowEnabled()) {
-      GlStateManager.depthFunc(GL11.GL_EQUAL);
+    if(this.sandbox$firstDepthCall) {
+      if(GlintColorizerAddon.getItemEffect() != ItemEffect.GLOW) {
+        GlStateManager.depthFunc(GL11.GL_EQUAL);
+      }
     } else {
       GlStateManager.depthFunc(i);
     }
