1feea2a556da723510a757ce0d459873ebc12494
RappyTV <rappytv@rappytv.com>
2025-10-07 22:06:30 +0200

    feat: Add 1.17.1 implementation TODO: Bypass RenderType#create being private

---
+++ game-runner/src/v1_17_1/java/com/rappytv/glintcolorizer/v1_17_1/ColoredVertexConsumer.java
@@ -0,0 +1,75 @@
+package com.rappytv.glintcolorizer.v1_17_1;
+
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.labymod.api.util.Color;
+
+public class ColoredVertexConsumer implements VertexConsumer {
+
+  private final VertexConsumer delegate;
+  private final float red;
+  private final float green;
+  private final float blue;
+
+  public ColoredVertexConsumer(VertexConsumer delegate, Color color) {
+    this.delegate = delegate;
+    this.red = color.getRed() / 255f;
+    this.green = color.getGreen() / 255f;
+    this.blue = color.getBlue() / 255f;
+  }
+
+  @Override
+  public VertexConsumer vertex(double x, double y, double z) {
+    this.delegate.vertex(x, y, z);
+    return this;
+  }
+
+  @Override
+  public VertexConsumer color(int red, int green, int blue, int alpha) {
+    this.delegate.color(
+        (int) (red * this.red),
+        (int) (green * this.green),
+        (int) (blue * this.blue),
+        alpha
+    );
+    return this;
+  }
+
+  @Override
+  public VertexConsumer uv(float u, float v) {
+    this.delegate.uv(u, v);
+    return this;
+  }
+
+  @Override
+  public VertexConsumer overlayCoords(int u, int v) {
+    this.delegate.overlayCoords(u, v);
+    return this;
+  }
+
+  @Override
+  public VertexConsumer uv2(int u, int v) {
+    this.delegate.uv2(u, v);
+    return this;
+  }
+
+  @Override
+  public VertexConsumer normal(float x, float y, float z) {
+    this.delegate.normal(x, y, z);
+    return this;
+  }
+
+  @Override
+  public void endVertex() {
+    this.delegate.endVertex();
+  }
+
+  @Override
+  public void defaultColor(int red, int green, int blue, int alpha) {
+    this.delegate.defaultColor(red, green, blue, alpha);
+  }
+
+  @Override
+  public void unsetDefaultColor() {
+    this.delegate.unsetDefaultColor();
+  }
+}
